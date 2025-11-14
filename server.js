99185c683e7f0708d1d0be7e3b84cd5e3d3ce345
GregoryTaran <34890989+GregoryTaran@users.noreply.github.com>
2025-11-14 01:05:53 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,47 @@
-const express = require("express");
-const http = require("http");
-const WebSocket = require("ws");
-const path = require("path");
-
-const app = express();
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-app.use(express.static(path.join(__dirname, "public")));
-
-wss.on("connection", ws => {
-  ws.on("message", msg => {
-    wss.clients.forEach(client => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(msg.toString());
-      }
-    });
-  });
-});
+const http = require('http');
+const fs = require('fs');
+const WebSocket = require('ws');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log("Server running on port " + PORT));
+
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath === './') filePath = './client.html';
+
+    const ext = path.extname(filePath);
+    const map = {
+        '.html': 'text/html',
+        '.js': 'text/javascript'
+    };
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(404);
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, { 'Content-Type': map[ext] || 'text/plain' });
+        res.end(data);
+    });
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', ws => {
+    console.log("Client connected");
+
+    ws.on('message', msg => {
+        console.log("Message from client:", msg.toString());
+
+        // Шлём ВСЕМ клиентам
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(msg.toString());
+            }
+        });
+    });
+});
+
+server.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
